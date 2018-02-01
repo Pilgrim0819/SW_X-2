ade0ddcb67141bd3220ff184e549a19ba9e0f77b
Csanád Vereckei <cxsanad@gmail.com>
2018-02-01 23:10:13 +0100

    Prefab instantiation upgrade

--- Assets/OnShipSelectionSceneLoad.cs
+++ Assets/OnShipSelectionSceneLoad.cs
@@ -6,13 +6,12 @@ using ShipsXMLCSharp;
 
 public class OnShipSelectionSceneLoad : MonoBehaviour {
 
-    private JSONLoader jsonLoader = new JSONLoader();
-
     public GameObject shipCard;
-
     public Transform shipCardPrefab;
-
     public GameObject cardsHolder;
+
+    private JSONLoader jsonLoader = new JSONLoader();
+    private const string IMAGE_FOLDER_NAME = "images";
 
 	// Use this for initialization
 	void Start () {
@@ -33,7 +32,7 @@ public class OnShipSelectionSceneLoad : MonoBehaviour {
 
         foreach(ShipsXMLCSharp.Ship ship in ships.Ship)
         {
-
+            Sprite shipSprite = Resources.Load<Sprite>(IMAGE_FOLDER_NAME + "/" + ship.ShipName.Replace("/", ""));
             Vector3 position = cardsHolder.transform.position;
             int offsetX = 150;
             int offsetY = -900;
@@ -41,12 +40,13 @@ public class OnShipSelectionSceneLoad : MonoBehaviour {
 
             Transform shipCard = (Transform) GameObject.Instantiate(
                 shipCardPrefab, 
-                new Vector3(position.x + (shipCardWidth * shipIndex) + offsetX,position.y + offsetY,position.z), 
+                new Vector3((position.x - 1000) + (shipCardWidth * shipIndex) + offsetX,position.y + offsetY,position.z), 
                 Quaternion.identity
             );
 
             shipCard.transform.SetParent(cardsHolder.transform, false);
             shipCard.transform.Find("Ship Name").gameObject.GetComponent<UnityEngine.UI.Text>().text = ship.ShipName.ToString();
+            shipCard.transform.Find("Ship Image").gameObject.GetComponent<Image>().sprite = shipSprite;
             shipCard.transform.Find("Attack Power Text").gameObject.GetComponent<UnityEngine.UI.Text>().text = ship.Weapon;
             shipCard.transform.Find("Agility Text").gameObject.GetComponent<UnityEngine.UI.Text>().text = ship.Agility;
             shipCard.transform.Find("Hull Text").gameObject.GetComponent<UnityEngine.UI.Text>().text = ship.Hull;
