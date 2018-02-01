1c9c89d65b1eb6509cfe60714a31c9f259bc4e6e
Csanád Vereckei <cxsanad@gmail.com>
2018-02-01 21:47:57 +0100

    Ship card prefab
    Script now can create the appropiate ship cards using prefab;

--- Assets/OnShipSelectionSceneLoad.cs
+++ Assets/OnShipSelectionSceneLoad.cs
@@ -8,6 +8,12 @@ public class OnShipSelectionSceneLoad : MonoBehaviour {
 
     private JSONLoader jsonLoader = new JSONLoader();
 
+    public GameObject shipCard;
+
+    public Transform shipCardPrefab;
+
+    public GameObject cardsHolder;
+
 	// Use this for initialization
 	void Start () {
         string chosenSide = PlayerDatas.getChosenSide();
@@ -23,9 +29,30 @@ public class OnShipSelectionSceneLoad : MonoBehaviour {
                 break;
         }
 
+        int shipIndex = 0;
+
         foreach(ShipsXMLCSharp.Ship ship in ships.Ship)
         {
-            
+
+            Vector3 position = cardsHolder.transform.position;
+            int offsetX = 150;
+            int offsetY = -900;
+            int shipCardWidth = 200;
+
+            Transform shipCard = (Transform) GameObject.Instantiate(
+                shipCardPrefab, 
+                new Vector3(position.x + (shipCardWidth * shipIndex) + offsetX,position.y + offsetY,position.z), 
+                Quaternion.identity
+            );
+
+            shipCard.transform.SetParent(cardsHolder.transform, false);
+            shipCard.transform.Find("Ship Name").gameObject.GetComponent<UnityEngine.UI.Text>().text = ship.ShipName.ToString();
+            shipCard.transform.Find("Attack Power Text").gameObject.GetComponent<UnityEngine.UI.Text>().text = ship.Weapon;
+            shipCard.transform.Find("Agility Text").gameObject.GetComponent<UnityEngine.UI.Text>().text = ship.Agility;
+            shipCard.transform.Find("Hull Text").gameObject.GetComponent<UnityEngine.UI.Text>().text = ship.Hull;
+            shipCard.transform.Find("Shield Text").gameObject.GetComponent<UnityEngine.UI.Text>().text = ship.Shield;
+
+            shipIndex++;
         }
 	}
 	
